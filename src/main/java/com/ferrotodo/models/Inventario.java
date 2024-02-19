ce1ae984a84291db200727b39d1998509c5aee39
Noonexyz1 <aries-d2github@hotmail.com>
2024-02-19 12:07:40 -0400

    Mapeado de las tablas de la BD terminado

--- src/main/java/com/ferrotodo/models/Inventario.java
+++ src/main/java/com/ferrotodo/models/Inventario.java
@@ -1,10 +1,20 @@
 package com.ferrotodo.models;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Inventario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_ferreteria")
     private Ferreteria ferreteria;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_producto")
     private Producto producto;
 
 
--- src/main/java/com/ferrotodo/models/Producto.java
+++ src/main/java/com/ferrotodo/models/Producto.java
@@ -1,22 +1,30 @@
 package com.ferrotodo.models;
 
 import com.ferrotodo.domain.enumer.TipoProducto;
+import jakarta.persistence.*;
 
 import java.util.List;
 
+@Entity
 public class Producto {
     //ESTRATEGIA EVERGREEN, osea, que todos los pasos funcionaran si o si
     //un producto tiene un margen de rentabilidad (buena utilidad)
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String nombre;
+    @Enumerated(EnumType.STRING)
     private TipoProducto categoria;
     private boolean tieneServicioTec;
     private boolean tieneRepuestos;
 
-
+    @OneToMany(mappedBy = "producto", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Inventario> inventarioList;
+
+    @OneToMany(mappedBy = "producto", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Proveedor> proveedorsList;
+
+    @OneToMany(mappedBy = "producto", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Transaccion> transaccionList;
 
 }
